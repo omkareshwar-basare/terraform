f4dbed1667fa4bf34900ced1f4175a0be4360472
Basareom <ombasare@gmail.com>
2026-02-21 00:32:07 +0530

    conditions

---
+++ conditions/variables.tf
@@ -0,0 +1,60 @@
+variable "environment" {
+    default = "dev"
+}
+
+
+
+variables "ami_id" {
+    type = string
+    default = "ami-0220d79f3f480ecf5"
+    description = "RHEL 9 Image"
+}
+
+variable "instance_type" {
+    type = string
+    default = "t3.micro"
+}
+
+variable "ec2_tags" {
+    type = map
+    default = {
+        Name = "variable-demo"
+        Project = "roboshop"
+        Terraform = "true"
+        Environmet = "dev"
+    }
+}
+
+variable "sg_name" {
+    default = "allow-all-terraform"
+    type = string
+}
+
+variable "sg_description" {
+    default = "Allow TLS inbound traffic and all outbound traffic"
+}
+
+variable "sg_from_port" {
+    type = number
+    default = 0
+}
+
+variable "sg_to_port" {
+    type = number
+    default = 0
+}
+
+variable "cidr_blocks" {
+    type = list
+    default = ["0.0.0.0/0"]
+}
+
+variable "sg_tags" {
+    type = map
+    default = {
+        Name = "allow-all-terraform"
+        Project = "roboshop"
+        Terraform = "true"
+        Environmet = "dev"
+    }
+}
